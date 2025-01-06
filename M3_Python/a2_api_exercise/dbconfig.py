8e8bcc96c1ce69c1dd11310cecb1029b046b07a3
Ayush <rishiustaad@gmail.com>
2025-01-06 14:58:26 +0530

    added assignment 1,2,3

---
+++ M3_Python/a2_api_exercise/dbconfig.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+def initializeDB():
+    connection = sqlite3.connect("./M3_Python/a2_api_exercise/books.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS books (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   title TEXT NOT NULL,
+                   author TEXT NOT NULL,
+                   published_year INTEGER NOT NULL,
+                   genre TEXT NOT NULL
+                   )
+                   """)
+    
+    sample_data = [
+        ("The Great Gatsby", "F. Scott Fitzgerald", 1925, "Fiction"),
+        ("To Kill a Mockingbird", "Harper Lee", 1960,  "Fiction"),
+        ("1984", "George Orwell", 1949,  "Fiction"),
+        ("Dune", "Frank Herbert", 1965, "Sci-Fi"),
+    ]
+
+    cursor.executemany(""" INSERT INTO books (title, author, published_year, genre) VALUES (?, ?, ?, ?)""", sample_data)
+
+    connection.commit()
+    connection.close()
+    print("Database initialized and sample data inserted.")
+
+if __name__ == "__main__":
+    initializeDB() 
